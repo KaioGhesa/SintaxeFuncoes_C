0e6344ce82e47b1c88284ae8ea303482439ae437
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-06-12 12:02:00 -0300

    Add files via upload

---
+++ SintaxeFuncoes/SintaxeFuncoes/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Digite três numeros");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+double resultado = Maior(n1, n2, n3);
+
+Console.WriteLine("Maior= " + resultado);
